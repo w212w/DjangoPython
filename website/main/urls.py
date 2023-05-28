a7ec5a3fb456fca4b4145face85510c914d07d8f
Билал Эдаев <edaev.bilal@rambler.ru>
2023-05-29 01:55:05 +0300

    Добавил подраздел "контакты" с функциональной частью

--- website/main/urls.py
+++ website/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path("", views.index, name="home"),
   path("about", views.about, name="about"),
+  path("contacts", views.contacts, name="contacts"),
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    data = {
-        "title": "Главная страница",
-        "values": ["some", "hello", "1234"],
-        "obj": {
-            "car": "BMW",
-            "age": "2020",
-            "hobby": "basketball",
-        }
-    }
-    return render(request, "main/index.html", data )
+    data = {"title": "Главная страница",}
+    return render(request, "main/index.html",data)
 
 def about(request):
     return render(request, "main/about.html")
+
+
+def contacts(request):
+    return render(request, "main/contacts.html")
