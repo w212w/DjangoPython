f730fab45ef229f872ef7523f157653e057e392c
Билал Эдаев <edaev.bilal@rambler.ru>
2023-05-30 01:28:52 +0300

    Добавил удаление и редактирование новостей

--- website/news/views.py
+++ website/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = Articles.objects.order_by("-date")
@@ -12,6 +12,19 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = "news/details_view.html"
     context_object_name = "article"
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = "news/create.html"
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = "/news/"
+    template_name = "news/news-delete.html"
 
 
 def create(request):
