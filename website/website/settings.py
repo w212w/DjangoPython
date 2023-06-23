e3c01a716302b0f8c3ac448e5c612e55729f3bf1
Билал Эдаев <edaev.bilal@rambler.ru>
2023-06-23 21:18:16 +0300

    Поменял "DEBUG = FALSE"

--- website/website/settings.py
+++ website/website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nxta6ns9#j1xkdoxo!k$d-srm)+d+uaxd3@wzckuh_mpq-nbs1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
